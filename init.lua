8699a5dd9913506677bcc5a13cc9408c475290a2
Briseglace <briseglace360@hotmail.fr>
2025-08-15 10:46:06 -0400

    Switched theme to kanagawa-dragon

--- init.lua
+++ init.lua
@@ -798,22 +798,23 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'EdenEast/nightfox.nvim',
+    'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('nightfox').setup {
+      require('kanagawa').setup {
         styles = {
           comments = {}, --italic = false }, -- Disable italics in comments
         },
       }
 
       -- Load the colorscheme here.
-      vim.cmd.colorscheme 'carbonfox'
+      vim.cmd.colorscheme 'kanagawa-dragon'
     end,
   },
   -- Additional themes.
   { 'folke/tokyonight.nvim' },
+  { 'EdenEast/nightfox.nvim' },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
